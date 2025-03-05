5d0cfa3aa06a1b4f6bcbec51231097a2a4848685
neru <ngocngocthuc@gmail.com>
2025-03-05 08:35:20 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters';
 
 async function bootstrap() {
+  Logger.log('DB Environment: ' + process.env.DB_HOST, 'Bootstrap');
+
   const app = await NestFactory.create(MainModule, {
     cors: true,
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
-  
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
@@ -24,8 +26,10 @@ async function bootstrap() {
   SwaggerModule.setup('/', app, document, {
     swaggerOptions: { defaultModelsExpandDepth: -1 },
   });
-
   await app.listen(process.env.PORT ?? 3000);
-  Logger.log(`Server running on http://localhost:${process.env.PORT ?? 3000}`, 'Bootstrap');
+  Logger.log(
+    `Server running on http://localhost:${process.env.PORT ?? 3000}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
