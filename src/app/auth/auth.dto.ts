fc347d1d01acadcf992ca10c7ab03533938718d1
neru <ngocngocthuc@gmail.com>
2025-03-25 10:12:30 +0700

    return type of authen

--- src/app/auth/auth.dto.ts
+++ src/app/auth/auth.dto.ts
@@ -2,50 +2,49 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SignInRequestBody {
-    @ApiProperty()
-    @IsEmail()
-    email: string;
+  @ApiProperty()
+  @IsEmail()
+  email: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    password: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  password: string;
 }
 
 export class SignUpRequestBody {
-    @ApiProperty()
-    @IsEmail()
-    email: string;
+  @ApiProperty()
+  @IsEmail()
+  email: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    password: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  password: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    fullname: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  fullname: string;
 
-    @ApiProperty()
-    phoneNumber?: string;
+  @ApiProperty()
+  phoneNumber?: string;
 }
 
 export class RefreshRequestBody {
-    @ApiProperty()
-    refreshToken: string;
+  @ApiProperty()
+  refreshToken: string;
 }
 
 export class AssignResponseData {
-    @ApiProperty()
-    token: {
-        accessToken: string;
-        refreshToken: string;
-    }
+  @ApiProperty()
+  accessToken: string;
+  @ApiProperty()
+  refreshToken: string;
 }
 
-export class GoogleSignInRequestBody{
-    @ApiProperty()
-    email: string;
-    @ApiProperty()
-    username: string;
-    @ApiProperty()
-    phoneNumber: string;
+export class GoogleSignInRequestBody {
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  username: string;
+  @ApiProperty()
+  phoneNumber: string;
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,63 +4,81 @@ import { User } from 'src/common/models';
 import { Repository } from 'typeorm';
 
 import * as bcrypt from 'bcrypt';
-import { BadRequestException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
-import { SignInRequestBody, SignUpRequestBody, AssignResponseData, RefreshRequestBody, GoogleSignInRequestBody } from './auth.dto';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  SignInRequestBody,
+  SignUpRequestBody,
+  AssignResponseData,
+  RefreshRequestBody,
+  GoogleSignInRequestBody,
+} from './auth.dto';
 import { ItemBaseResponse } from 'src/common/base';
 import { emit } from 'process';
 
 @Injectable()
 export class AuthService {
-  private readonly DEFAULT_GOOGLE_PASSWORD = "User@12345";
+  private readonly DEFAULT_GOOGLE_PASSWORD = 'User@12345';
   constructor(
     private readonly jwtService: JwtService,
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  async signIn(body: SignInRequestBody): Promise<ItemBaseResponse<AssignResponseData>> {
+  async signIn(
+    body: SignInRequestBody,
+  ): Promise<ItemBaseResponse<AssignResponseData>> {
     this.validatePassword(body.password);
     const user = await this.userRepository.findOneBy({ email: body.email });
     if (!user) throw new NotFoundException('This email does not exist');
-    if (user.isActive === false) throw new BadRequestException('This account has been deactivated');
-    if (user.isDeleted === true) throw new BadRequestException('This account has been deleted');
+    if (user.isActive === false)
+      throw new BadRequestException('This account has been deactivated');
+    if (user.isDeleted === true)
+      throw new BadRequestException('This account has been deleted');
     const isPasswordMatch = await bcrypt.compare(body.password, user.password);
-    if (!isPasswordMatch) throw new BadRequestException('Wrong login credentials');
+    if (!isPasswordMatch)
+      throw new BadRequestException('Wrong login credentials');
     const payload = {
       id: user.id,
       email: user.email,
       avatar: user.avatar,
       role: user.role,
       fullname: user.fullname,
-    }
+    };
     const accessToken = await this.jwtService.signAsync(payload, {
       expiresIn: +process.env.JWT_ACCESS_TOKEN_EXPIRATION,
       secret: process.env.JWT_ACCESS_TOKEN_SECRET,
-    })
+    });
     const refreshToken = await this.jwtService.signAsync(payload, {
       expiresIn: +process.env.JWT_REFRESH_TOKEN_EXPIRATION,
       secret: process.env.JWT_REFRESH_TOKEN_SECRET,
-    })
+    });
     return {
       status: HttpStatus.OK,
-      data:{
-        token: {
-          accessToken,
-          refreshToken
-        }
+      data: {
+        accessToken,
+        refreshToken,
       },
-      message: 'Assign successfully'
-    }
+      message: 'Assign successfully',
+    };
   }
 
-  async google(body: GoogleSignInRequestBody): Promise<ItemBaseResponse<AssignResponseData>> {
-    const user = await this.userRepository.findOne({where:{email: body.email}});
+  async google(
+    body: GoogleSignInRequestBody,
+  ): Promise<ItemBaseResponse<AssignResponseData>> {
+    const user = await this.userRepository.findOne({
+      where: { email: body.email },
+    });
     if (!user) {
       return await this.signUp({
         email: body.email,
         fullname: body.username,
         password: this.DEFAULT_GOOGLE_PASSWORD,
-        phoneNumber: body.phoneNumber
-      })
+        phoneNumber: body.phoneNumber,
+      });
     }
     const payload = {
       id: user.id,
@@ -68,28 +86,28 @@ export class AuthService {
       avatar: user.avatar,
       role: user.role,
       fullname: user.fullname,
-    }
+    };
     const accessToken = await this.jwtService.signAsync(payload, {
       expiresIn: +process.env.JWT_ACCESS_TOKEN_EXPIRATION,
       secret: process.env.JWT_ACCESS_TOKEN_SECRET,
-    })
+    });
     const refreshToken = await this.jwtService.signAsync(payload, {
       expiresIn: +process.env.JWT_REFRESH_TOKEN_EXPIRATION,
       secret: process.env.JWT_REFRESH_TOKEN_SECRET,
-    })
+    });
     return {
       status: HttpStatus.OK,
-      data:{
-        token: {
-          accessToken,
-          refreshToken
-        }
+      data: {
+        accessToken,
+        refreshToken,
       },
-      message: 'Assign successfully'
-    }
+      message: 'Assign successfully',
+    };
   }
 
-  async signUp(body: SignUpRequestBody): Promise<ItemBaseResponse<AssignResponseData>> {
+  async signUp(
+    body: SignUpRequestBody,
+  ): Promise<ItemBaseResponse<AssignResponseData>> {
     this.validatePassword(body.password);
     const user = await this.userRepository.findOneBy({ email: body.email });
     if (user) throw new BadRequestException('This email already exists');
@@ -100,7 +118,9 @@ export class AuthService {
     return await this.signIn({ email: body.email, password });
   }
 
-  async refresh(body : RefreshRequestBody) : Promise<ItemBaseResponse<AssignResponseData>> {
+  async refresh(
+    body: RefreshRequestBody,
+  ): Promise<ItemBaseResponse<AssignResponseData>> {
     const payload = await this.jwtService.verifyAsync(body.refreshToken, {
       secret: process.env.JWT_REFRESH_TOKEN_SECRET,
     });
@@ -118,38 +138,46 @@ export class AuthService {
     return {
       status: HttpStatus.OK,
       data: {
-        token: {
-          accessToken,
-          refreshToken
-        }
+        accessToken,
+        refreshToken,
       },
-      message: 'Refresh token successfully'
-    }
+      message: 'Refresh token successfully',
+    };
   }
 
-  async me(token: string):Promise<ItemBaseResponse<User>> {
+  async me(token: string): Promise<ItemBaseResponse<User>> {
     const payload = await this.jwtService.verifyAsync(token, {
       secret: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
     if (!payload) throw new BadRequestException('Invalid token');
-    const user = await this.userRepository.findOne({ where: { email: payload.email } });
+    const user = await this.userRepository.findOne({
+      where: { email: payload.email },
+    });
     return {
       status: HttpStatus.OK,
       data: user,
-      message: 'Get user information successfully'
-    }
+      message: 'Get user information successfully',
+    };
   }
 
   private validatePassword(password: string): void {
-    if (password.length < 8) 
+    if (password.length < 8)
       throw new BadRequestException('Password must be at least 8 characters');
     if (!/[A-Z]/.test(password))
-      throw new BadRequestException('Password must contain at least one uppercase letter');
+      throw new BadRequestException(
+        'Password must contain at least one uppercase letter',
+      );
     if (!/[a-z]/.test(password))
-      throw new BadRequestException('Password must contain at least one lowercase letter');
+      throw new BadRequestException(
+        'Password must contain at least one lowercase letter',
+      );
     if (!/[0-9]/.test(password))
-      throw new BadRequestException('Password must contain at least one number');
+      throw new BadRequestException(
+        'Password must contain at least one number',
+      );
     if (!/[!@#$%^&*]/.test(password))
-      throw new BadRequestException('Password must contain at least one special character');
+      throw new BadRequestException(
+        'Password must contain at least one special character',
+      );
   }
 }
